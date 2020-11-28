a3884f60ab30bb0225820de6712cca4b0d95dee4
Josh Payne <paynejoshua1@gmail.com>
2020-11-28 10:56:44 -0800

    got service worker file registered and working

---
+++ public/service-worker.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const staticFilesToPreCache = [
+    "/",
+    "favicon.ico",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/index.html",
+    "/assets/index.js",
+    "/assets/db.js",
+    "/styles.css"
+  ];
+
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(staticFilesToPreCache);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  // activate
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function(evt) {
+    const {url} = evt.request;
+    if (url.includes("/api/transaction") || url.includes("/api/transaction/bulk")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then((cache) => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+    } else {
+      // respond from static cache, request is not for /api/*
+      evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+          return cache.match(evt.request).then((response) => {
+            return response || fetch(evt.request);
+          });
+        })
+      );
+    }
+  });
